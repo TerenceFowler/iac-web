e10fd58a39cd073cd2934b56d10b664612f4ad90
TerenceFowler <44010313+TerenceFowler@users.noreply.github.com>
2018-10-16 21:19:25 -0400

    Update resetWebSite.sh

--- resetWebSite.sh
+++ resetWebSite.sh
@@ -2,6 +2,5 @@
 sudo git clone https://github.com/TerenceFowler/iac-web.git
 sudo cp -a /home/ec2-user/iac-web/. /var/www/html/
 sudo rm -rf /home/ec2-user/iac-web/
-You have new mail in /var/spool/mail/ec2-user
-[ec2-user@ip-172-31-25-30 ~]$ 
+
 
